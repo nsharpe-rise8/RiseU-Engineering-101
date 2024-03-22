33b4dcac4c934ff7a0d7f497bccd3fc3debdf0de
Norman Sharpe <nsharpe@rise8.us>
2024-03-21 22:15:29 -1000

    added openai functionality

--- application/riser-chat-api/src/app.module.ts
+++ application/riser-chat-api/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OpenaiController } from './openai/openai.controller';
+import { OpenaiService } from './openai/openai.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController, OpenaiController],
+  providers: [AppService, OpenaiService],
 })
 export class AppModule {}
