645c64cad912726edc9486309154d3bbe1569b3b
Norman Sharpe <nsharpe@rise8.us>
2024-03-21 23:17:03 -1000

    created chat functionality.

--- application/riser-chat-api/src/main.ts
+++ application/riser-chat-api/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // Replace with the origin of your frontend app
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true, // if your frontend needs to send cookies
+  });
+
   await app.listen(3000);
 }
 bootstrap();
